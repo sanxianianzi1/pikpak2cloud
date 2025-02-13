6e13a306ae214582df82176b84ed35a8a27937ca
sanxianianzi1 <sanxianianzi@gmail.com>
2025-02-13 18:34:36 +0800

    Update task.py

--- task.py
+++ task.py
@@ -13,19 +13,15 @@ from sqlalchemy import (
     Text,
     create_engine,
 )
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 from sqlalchemy.sql import func
-
 
 parser = argparse.ArgumentParser(description="操作数据库")
 parser.add_argument("--opt", help="操作", default="query")
 parser.add_argument("--con", help="数据库链接地址", default="")
 parser.add_argument("--name", help="文件名称", default="")
 
-
 args = parser.parse_args()
-
 
 engine = create_engine(
     args.con,
@@ -36,7 +32,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 db = SessionLocal()
 Base = declarative_base()
 
-
 class Task(Base):
     __tablename__ = "task"
 
@@ -46,15 +41,16 @@ class Task(Base):
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     url = Column(String)
 
-
 def find_one_and_update():
     with Session(engine) as session:
         task = db.query(Task).filter(Task.status == "draft").first()
+        if task is None:
+            print("没有找到待处理的任务")
+            sys.exit(1)
         task.status = "published"
         db.commit()
         db.refresh(task)
         return task
-
 
 def delete_task():
     with Session(engine) as session:
@@ -64,22 +60,11 @@ def delete_task():
             db.delete(task)
             db.commit()
 
-
 if __name__ == "__main__":
     if args.opt == "query":
         task = find_one_and_update()
-        if task is None:
-            print("没有找到相关记录")
-            quit()
-        urlinfo = task.url.split("##")
-        cmd = (
-            "aria2c --conf aria2.conf --seed-time=0 -o "
-            + urlinfo[1]
-            + ' -d downloads -c "'
-            + urlinfo[0]
-            + '"'
-        )
-        os.system(cmd)
-        quit()
+        if task is not None:
+            print(task.url)
+        sys.exit(0)
     if args.opt == "delete":
         delete_task()
